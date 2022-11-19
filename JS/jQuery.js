991687aff8e567bca580c5a19d45690a3bf4889c
urassh <84073765+urassh@users.noreply.github.com>
2022-11-19 16:29:51 +0900

    遷移動作を実装した。

--- JS/jQuery.js
+++ JS/jQuery.js
@@ -4,5 +4,11 @@ jQuery(function() {
             changeDisplay('view');
         }
     );
-  
+    $('li').click(
+        function(e) {
+            let id = $(this).attr('id');
+            selected_product = id;
+            window.location.href = "../View/product_info.html";
+        }
+    );
 });
